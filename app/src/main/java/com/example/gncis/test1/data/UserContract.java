d464a8017167ef2c9857c9c52510d37f322d1c4d
GabrielDaKing <GabrielDaKing@users.noreply.github.com>
2018-04-04 19:53:13 +0530

    lah

--- app/src/main/java/com/example/gncis/test1/data/UserContract.java
+++ app/src/main/java/com/example/gncis/test1/data/UserContract.java
@@ -12,7 +12,7 @@ public final class UserContract {
 
     public static final class UserEntry implements BaseColumns{
 
-        public static final String TABLE_NAME = "flights";
+        public static final String TABLE_NAME = "users";
         public static final String USER = "User";
         public static final String USER_ID= BaseColumns._ID;
 
