1cf6cf9c34a5af540fe546b3d74ef0b66e0fd869
SamDaQueen <samreenreyaz@outlook.com>
2018-04-20 02:07:18 +0530

    Listview

--- app/src/main/java/com/example/gncis/test1/ViewCurrent.java
+++ app/src/main/java/com/example/gncis/test1/ViewCurrent.java
@@ -1,22 +1,13 @@
 package com.example.gncis.test1;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.example.gncis.test1.data.UserContract;
 import com.example.gncis.test1.data.UserDBHelper;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class ViewCurrent extends AppCompatActivity {
 
@@ -43,19 +34,18 @@ public class ViewCurrent extends AppCompatActivity {
         tripAdapter = new TripAdapter(this, R.layout.user_tile, trips);
         listView.setAdapter(tripAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                final Trip trip = tripAdapter.getItem(position);
-                Bundle bundle = new Bundle();
-                bundle.putString("name", trip.getTripName());
-                bundle.putString("sdate", trip.gettSDate());
-                bundle.putString("edate", trip.gettEDate());
-                startActivity(new Intent(ViewCurrent.this,DisplayParts.class).putExtra("trip", bundle));
-
-            }
-        });
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                final Trip trip = tripAdapter.getItem(position);
+//                Bundle bundle = new Bundle();
+//                bundle.putString("name", trip.getTripName());
+//                bundle.putString("sdate", trip.gettSDate());
+//                bundle.putString("edate", trip.gettEDate());
+//                startActivity(new Intent(ViewCurrent.this,DisplayParts.class).putExtra("trip", bundle));
+//            }
+//        });
 
     }
 }
