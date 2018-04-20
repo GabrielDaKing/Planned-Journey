f9b23e8627e9ef6d644960c6b33d7a910c5d82d9
N00BSlayer <37168727+Tokzek@users.noreply.github.com>
2018-04-21 02:11:18 +0530

    HotelDB ROOMTYPE FIX

--- app/src/main/java/com/example/gncis/test1/Hotel.java
+++ app/src/main/java/com/example/gncis/test1/Hotel.java
@@ -66,7 +66,7 @@ public class Hotel {
         return hRoomType;
     }
 
-    public void sethRoomType(int hRoomType) {
+    public void sethRoomType(String hRoomType) {
         this.hRoomType = hRoomType;
     }
 }
